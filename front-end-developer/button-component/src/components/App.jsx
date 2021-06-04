383783b610c60bae75876d055bd48b5c1af79d19
Amy Dang <kirontoo@gmail.com>
2021-06-03 22:46:21 -0700

    fix tabbing

--- front-end-developer/button-component/src/components/App.jsx
+++ front-end-developer/button-component/src/components/App.jsx
@@ -5,40 +5,58 @@ import Button from './Button';
 // - button page
 // - button component
 //		- props: text, size, color, variant, disabled
+//		TODO: labels for the buttons
 
 function App() {
 	const navLinks = [ "colors", "typography", "spaces", "buttons", "inputs", "grid" ];
   return (
-		<main className="app">
-			<aside className="sidebar">
-				<span className="logo">
-					<span className="orange">dev</span><span>challenges.io</span>
-				</span>
-				<Nav links={navLinks}/>
-			</aside>
-			<section className="content">
-				<h1>buttons</h1>
-				<div className="button-list">
-					<div className="row">
-						<Button color="primary">Primary</Button>
-						<Button color="secondary">Secondary</Button>
-						<Button color="danger">Danger</Button>
-						<Button>Default</Button>
-					</div>
-					<div className="row">
-						<Button variant="text">Variant text</Button>
-						<Button variant="outline">Variant outline</Button>
-					</div>
-					<div className="row">
-						<Button color="primary">Primary</Button>
-						<Button disabled>Disabled</Button>
-						<Button startIcon="shopping_cart">Default</Button>
-						<Button endIcon="shopping_cart">Default</Button>
-						<Button disabledShadow>no shadow</Button>
-					</div>
-				</div>
-			</section>
-		</main>
+    <main className="app">
+      <aside className="sidebar">
+	<span className="logo">
+	  <span className="orange">dev</span><span>challenges.io</span>
+	</span>
+	<Nav links={navLinks}/>
+      </aside>
+      <section className="content">
+	<h1>buttons</h1>
+	<div className="button-list">
+	  <div className="row">
+	    <Button>Default</Button>
+	  </div>
+	  <div className="row">
+	    <Button variant="outline">Variant outline</Button>
+	    <Button variant="outline">Variant outline</Button>
+	  </div>
+	  <div className="row">
+	    <Button variant="text">Variant text</Button>
+	    <Button variant="text">Variant text</Button>
+	  </div>
+	  <div className="row">
+	    <Button color="primary" disabledShadow>no shadow</Button>
+	  </div>
+	  <div className="row">
+	    <Button disabled>Disabled</Button>
+	    <Button variant="text" disabled>Disabled</Button>
+	  </div>
+	  <div className="row">
+	    <Button color="primary" startIcon="shopping_cart">Default</Button>
+	    <Button color="primary" endIcon="shopping_cart">Default</Button>
+	  </div>
+	  <div className="row">
+	    <Button>Default</Button>
+	    <Button color="primary">Primary</Button>
+	    <Button color="secondary">Secondary</Button>
+	    <Button color="danger">Danger</Button>
+	  </div>
+	  <div className="row">
+	    <Button>Default</Button>
+	    <Button color="primary">Primary</Button>
+	    <Button color="secondary">Secondary</Button>
+	    <Button color="danger">Danger</Button>
+	  </div>
+	</div>
+      </section>
+    </main>
   );
 }
 
