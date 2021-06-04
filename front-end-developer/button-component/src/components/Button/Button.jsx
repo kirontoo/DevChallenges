32c0b85afa7dbc2f989a754b811f6741f5921849
Amy Dang <kirontoo@gmail.com>
2021-06-04 16:56:22 -0700

    fixed disabled color and row gap

--- front-end-developer/button-component/src/components/Button/Button.jsx
+++ front-end-developer/button-component/src/components/Button/Button.jsx
@@ -2,8 +2,6 @@ import styled, { css } from 'styled-components';
 
 /**
 * options:
-*   variant: outline, text
-*   disabled
 *   size: sm md lg (change padding)
 * */
 
@@ -11,14 +9,15 @@ import styled, { css } from 'styled-components';
 
 const Btn = ({ className, children, startIcon, endIcon }) => {
 	return (
-	<button className={className}>
-		{ startIcon ? <span className="material-icons">{ startIcon }</span> : undefined }
-		{ children }
-		{ endIcon ? <span className="material-icons">{ endIcon }</span> : undefined }
-	</button>
+		<button className={className}>
+			{ startIcon ? <span className="material-icons">{ startIcon }</span> : undefined }
+			{ children }
+			{ endIcon ? <span className="material-icons">{ endIcon }</span> : undefined }
+		</button>
 )};
 
-const setColor = ( { color, variant } ) => { 
+const setColor = ( { disabled, color, variant } ) => { 
+	if ( disabled ) { return 'rgba(var(--disabled-color), 1)' }
 	return ( variant && color ) ? `rgba(var(--${color}-color), 1)` : ( color ) ? 'white' : 'rgba(var(--default-text-color))';
 };
 
@@ -30,7 +29,6 @@ const setBackgroundColor =  ( { color, variant } ) => {
 			return 'rgba(var(--default-color), 1)';
 	}
 }
-
 
 const setHover =({ color, variant }) => {
 	let styles = '';
@@ -61,18 +59,30 @@ const setBorder = ({ color, variant }) => {
 	}
 }
 
+const setPadding = ({ size }) => {
+	switch ( size ) {
+	case 'md':
+		return '0.5em 1.2em';
+	case 'lg':
+		return '0.5em 1.8em';
+	default: 
+		return '0.5em 1em';
+	}
+}
+
 // TODO: fix variant text shadow
 const Button = styled(Btn)`
 	font-family: 'Noto Sans JP', sans-serif;
 	font-weight: 500;
 	border-radius: 6px;
-	padding: 0.5em 1em;
 	text-align: center;
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	text-decoration: none;
+	max-height: 2.5em;
 
+	padding: ${ props => setPadding( props ) };
 	color: ${ props => setColor( props ) };
 	background: ${ props => setBackgroundColor( props ) };
 	border: ${ props => setBorder( props ) };
