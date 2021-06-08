74f383dbb9f446380f19a8f07be6db152b99aec3
Amy Dang <kirontoo@gmail.com>
2021-06-08 09:44:14 -0700

    change class name

--- front-end-developer/input-component/src/components/ButtonPage/ButtonPage.jsx
+++ front-end-developer/input-component/src/components/ButtonPage/ButtonPage.jsx
@@ -4,109 +4,109 @@ const ButtonPage = () => {
   return (
     <>
       <h1>buttons</h1>
-      <div className="button-list">
+      <div className="list">
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name">&lt;Button /&gt;</p>
+	    <p className="list-name">&lt;Button /&gt;</p>
 	    <Button>Default</Button>
 	  </div>
 	  <div className="column">
-	    <p className="button-name hover-text">&:hover, &:focus</p>
+	    <p className="list-name hover-text">&:hover, &:focus</p>
 	    <Button>Default</Button>
 	  </div>
 	</div>
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name">&lt;Button variant="outline"/&gt;</p>
+	    <p className="list-name">&lt;Button variant="outline"/&gt;</p>
 	    <Button color="primary" variant="outline">Default</Button>
 	  </div>
 	  <div className="column">
-	    <p className="button-name hover-text">&:hover, &:focus</p>
+	    <p className="list-name hover-text">&:hover, &:focus</p>
 	    <Button color="primary" variant="outline">Default</Button>
 	  </div>
 	</div>
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name">&lt;Button variant="text" /&gt;</p>
+	    <p className="list-name">&lt;Button variant="text" /&gt;</p>
 	    <Button color="primary" variant="text">Default</Button>
 	  </div>
 
 	  <div className="column">
-	    <p className="button-name hover-text">&:hover, &:focus</p>
+	    <p className="list-name hover-text">&:hover, &:focus</p>
 	    <Button color="primary" variant="text">Default</Button>
 	  </div>
 	</div>
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name">&lt;Button disabledShadow /&gt;</p>
+	    <p className="list-name">&lt;Button disabledShadow /&gt;</p>
 	    <Button color="primary" disableShadow>Default</Button>
 	  </div>
 	</div>
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name">&lt;Button disabled /&gt;</p>
+	    <p className="list-name">&lt;Button disabled /&gt;</p>
 	    <Button disabled>Disabled</Button>
 	  </div>
 	  <div className="column">
-	    <p className="button-name">&lt;Button variant="text" disabled /&gt;</p>
+	    <p className="list-name">&lt;Button variant="text" disabled /&gt;</p>
 	    <Button variant="text" disabled>Disabled text</Button>
 	  </div>
 	</div>
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name">&lt;Button startIcon="local_grocery_store" /&gt;</p>
+	    <p className="list-name">&lt;Button startIcon="local_grocery_store" /&gt;</p>
 	    <Button color="primary" startIcon="local_grocery_store">Default</Button>
 	  </div>
 
 	  <div className="column">
-	    <p className="button-name">&lt;Button endIcon="local_grocery_store" /&gt;</p>
+	    <p className="list-name">&lt;Button endIcon="local_grocery_store" /&gt;</p>
 	    <Button color="primary" endIcon="local_grocery_store">Default</Button>
 	  </div>
 	</div>
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name">&lt;Button size="sm"/&gt;</p>
+	    <p className="list-name">&lt;Button size="sm"/&gt;</p>
 	    <Button size="sm">Default</Button>
 	  </div>
 	  <div className="column">
-	    <p className="button-name">&lt;Button size="md"/&gt;</p>
+	    <p className="list-name">&lt;Button size="md"/&gt;</p>
 	    <Button size="md">Default</Button>
 	  </div>
 	  <div className="column">
-	    <p className="button-name">&lt;Button size="lg"/&gt;</p>
+	    <p className="list-name">&lt;Button size="lg"/&gt;</p>
 	    <Button size="lg">Default</Button>
 	  </div>
 	</div>
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name">&lt;Button /&gt;</p>
+	    <p className="list-name">&lt;Button /&gt;</p>
 	    <Button>Default</Button>
 	  </div>
 	  <div className="column">
-	    <p className="button-name">&lt;Button color="primary" /&gt;</p>
+	    <p className="list-name">&lt;Button color="primary" /&gt;</p>
 	    <Button color="primary" >Default</Button>
 	  </div>
 	  <div className="column">
-	    <p className="button-name">&lt;Button color="secondary" /&gt;</p>
+	    <p className="list-name">&lt;Button color="secondary" /&gt;</p>
 	    <Button color="secondary" >Default</Button>
 	  </div>
 	  <div className="column">
-	    <p className="button-name">&lt;Button color="danger" /&gt;</p>
+	    <p className="list-name">&lt;Button color="danger" /&gt;</p>
 	    <Button color="danger" >Default</Button>
 	  </div>
 	</div>
 
 	<div className="row">
 	  <div className="column">
-	    <p className="button-name hover-text">&:hover, &:focus</p>
+	    <p className="list-name hover-text">&:hover, &:focus</p>
 	    <Button>Default</Button>
 	  </div>
 	  <Button color="primary">Primary</Button>
