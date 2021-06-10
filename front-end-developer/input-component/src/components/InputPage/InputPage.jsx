060cb85a5153cc0e4f6f9f9a6f5af3c0b1282ee0
Amy Dang <kirontoo@gmail.com>
2021-06-09 21:39:06 -0700

    set up input page

--- front-end-developer/input-component/src/components/InputPage/InputPage.jsx
+++ front-end-developer/input-component/src/components/InputPage/InputPage.jsx
@@ -5,12 +5,98 @@ const InputPage = () => {
     <>
       <h1>input page</h1>
       <div className="list">
+
         <div className="row">
           <div className="column">
             <p className="list-name">&lt;Input/&gt;</p>
-            <Input label="Label" placeholder="Placeholder"></Input>
+            <Input label="Label" placeholder="Placeholder" />
+          </div>
+          <div className="column">
+	          <p className="list-name hover-text">&:hover</p>
+            <Input label="Label" placeholder="Placeholder" />
+          </div>
+          <div className="column">
+	          <p className="list-name hover-text">&:focus</p>
+            <Input label="Label" placeholder="Placeholder" focus />
           </div>
         </div>
+
+        <div className="row">
+          <div className="column">
+            <p className="list-name">&lt;Input error /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" error />
+          </div>
+          <div className="column">
+	          <p className="list-name hover-text">&:hover</p>
+            <Input label="Label" placeholder="Placeholder" error />
+          </div>
+          <div className="column">
+	          <p className="list-name hover-text">&:focus</p>
+            <Input label="Label" placeholder="Placeholder" error />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <p className="list-name">&lt;Input disabled /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" disabled/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <p className="list-name">&lt;Input helperText="Some interesting text" /&gt;</p>
+            <Input helperText="Some interesting text" label="Label" placeholder="Placeholder" />
+          </div>
+          <div className="column">
+            <p className="list-name">&lt;Input helperText="Some interesting text" error /&gt;</p>
+            <Input helperText="Some interesting text" label="Label" placeholder="Placeholder" error />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <p className="list-name">&lt;Input startIcon="mail" /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" endIcon="mail"/>
+          </div>
+          <div className="column">
+            <p className="list-name">&lt;Input endIcon="mail" /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" endIcon="mail"/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <p className="list-name">&lt;Input value="text" /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" value="text"/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <p className="list-name">&lt;Input size="sm" /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" size="sm"/>
+          </div>
+          <div className="column">
+            <p className="list-name">&lt;Input size="md" /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" size="md"/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <p className="list-name">&lt;Input fullWidth /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" fullWidth/>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="column">
+            <p className="list-name">&lt;Input multiline row="4" /&gt;</p>
+            <Input label="Label" placeholder="Placeholder" multiline row="4"/>
+          </div>
+        </div>
+
       </div>
     </>
   )
