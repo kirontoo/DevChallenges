89f18f7dd95f021c6944c2c492a2cf1ed02c2b7b
Amy Dang <kirontoo@gmail.com>
2021-06-13 16:30:42 -0700

    fix tabbing

--- front-end-developer/input-component/src/components/App.jsx
+++ front-end-developer/input-component/src/components/App.jsx
@@ -5,25 +5,25 @@ import InputPage from './InputPage';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
-  const navLinks = [ "colors", "typography", "spaces", "buttons", "inputs", "grid" ];
+	const navLinks = [ "colors", "typography", "spaces", "buttons", "inputs", "grid" ];
 
-  return (
-    <Router>
-      <main className="app">
-      	<aside className="sidebar">
-	  			<span className="logo">
-	    			<span className="orange">dev</span><span>challenges.io</span>
-	  			</span>
-	  			<Nav/>
-      	</aside>
-      	<section className="content">
-      	  <Route exact path="/" component={InputPage}/>
-          <Route path="/buttons" component={ButtonPage}/>
-          <Route path="/inputs" component={InputPage}/>
-      	</section>
-      </main>
-    </Router>
-  );
+	return (
+		<Router>
+			<main className="app">
+				<aside className="sidebar">
+					<span className="logo">
+						<span className="orange">dev</span><span>challenges.io</span>
+					</span>
+					<Nav/>
+				</aside>
+				<section className="content">
+					<Route exact path="/" component={InputPage}/>
+					<Route path="/buttons" component={ButtonPage}/>
+					<Route path="/inputs" component={InputPage}/>
+				</section>
+			</main>
+		</Router>
+	);
 }
 
 export default App;
