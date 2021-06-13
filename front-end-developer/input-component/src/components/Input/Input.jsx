7e46cad5efd51ff1f404cae2f0bfa33cb356d573
Amy Dang <kirontoo@gmail.com>
2021-06-13 16:27:07 -0700

    fix icon displaying over text, add multiline

--- front-end-developer/input-component/src/components/Input/Input.jsx
+++ front-end-developer/input-component/src/components/Input/Input.jsx
@@ -1,121 +1,130 @@
 import styled from 'styled-components';
 
-/*
-* Requirements:
-*   -- :focus -> label doesn't change color
-*   startIcon, endIcon
-*   multiline ( built-in )
-*   TODO: label on focus should be blue
-* */
-
-
-const Inpt = ({ startIcon, endIcon, multiline, ...rest }) => {
-  return <input {...rest} multiline={multiline}/>
+const Inpt = ({ multiline, row, ...rest }) => {
+	return multiline 
+		? <textarea {...rest} columns="60" rows={row} /> 
+		: <input {...rest} />
 };
 
 const setFocusColor = ({ error }) => {
-  return ( error ) ? 'rgba(var(--input-error-color), 1)' : 'rgba(var(--primary-color), 1)';
+	return ( error ) ? 'rgba(var(--input-error-color), 1)' : 'rgba(var(--primary-color), 1)';
 };
 
 const setBorderColor = ({ disabled, error }) => {
-  if ( disabled ) {
-    return 'rgba(var(--input-disabled-color), 1)';
-  } else if ( error ) {
-    return 'rgba(var(--input-error-color), 1)'; 
-  } else {
-    return 'rgba(var(--input-default-color), 1)'; 
-  }
+	if ( disabled ) {
+		return 'rgba(var(--input-disabled-color), 1)';
+	} else if ( error ) {
+		return 'rgba(var(--input-error-color), 1)'; 
+	} else {
+		return 'rgba(var(--input-default-color), 1)'; 
+	}
 };
 
 const setErrorColor = ({ error }) => {
-  return ( error ) ? 'rgba(var(--input-error-color), 1)' : 'rgba(var(--input-default-color), 1)';
+	return error 
+		? 'rgba(var(--input-error-color), 1)' 
+		: 'rgba(var(--input-default-color), 1)';
 }
 
 const setLabelColor = ({ error }) => {
-  return ( error ) ? 'rgba(var(--input-error-color), 1)' : 'rgba(var(--input-text-color), 1)';
+	return error 
+		? 'rgba(var(--input-error-color), 1)' 
+		: 'rgba(var(--input-text-color), 1)';
 }
 
 const setSize = ({ size }) => {
-  if ( size == 'md' ) {
-    return '1.3em 0.8em'
-  } else {
-    return '1em 0.8em'
-  }
+	if ( size == 'md' ) {
+		return '1.3em 0.8em'
+	} else {
+		return '1em 0.8em'
+	}
 }
 
 const StyledInput = styled(Inpt)`
-  font-family: 'Noto Sans JP', sans-serif;
-  font-weight: 500;
-  padding: ${ props => setSize(props) };
+	font-family: 'Noto Sans JP', sans-serif;
+	font-weight: 500;
+	border-radius: 0.5em;
+	color: rgba(var(--input-text-color), 1);
 
-  color: rgba(var(--input-text-color), 1);
-  width: ${ props => props.fullWidth ? '100%' : '15em'};
-  border: 1px solid ${props => setBorderColor( props )};
-  border-radius: 0.5em;
+	width: ${ props => props.fullWidth ? '100%' : '15em'};
+	border: 1px solid ${props => setBorderColor( props )};
+	padding: ${ props => setSize(props) };
+	padding-right: ${props => props.endIcon ? '2.8em' : '0.8em'};
 
-  ${ props => props.disabled ? 'background-color: rgba(var(--input-disabled-background-color), 1);' : '' }
+	${ props => props.disabled ? 'background-color: rgba(var(--input-disabled-background-color), 1);' : '' }
 
-  &::placeholder {
-    color: rgba(var( --input-default-color ), 1);
-  }
+	text-indent: ${ props => props.startIcon ? '1.8em' : '0' };
 
-  &:focus {
-    border-color: ${ props => setFocusColor( props ) };
-  }
+	&::placeholder {
+		color: rgba(var( --input-default-color ), 1);
+	}
 
-  &:hover {
-    border-color: rgba(var(--input-text-color), 1);
-  }
+	&:focus {
+		border-color: ${ props => setFocusColor( props ) };
+	}
+
+	&:hover {
+		border-color: rgba(var(--input-text-color), 1);
+	}
 `;
 
 const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	width: 100%;
 `;
 
 const Label = styled.label`
-  font-family: 'Noto Sans JP', sans-serif;
-  font-weight: 400;
-  font-size: 0.8em;
-  color: ${ props => setLabelColor(props) };
-  margin-bottom: 0.5em;
+	font-family: 'Noto Sans JP', sans-serif;
+	font-weight: 400;
+	font-size: 0.8em;
+	color: ${ props => setLabelColor(props) };
+	margin-bottom: 0.5em;
 
-  &:focus {
-    color: rgba(var(--primary-color), 1);
-  }
+	&:focus {
+		color: rgba(var(--primary-color), 1);
+	}
 `;
 
 const Icon = styled.i`
-  position: relative;
-  top: 1.65em;
-  left: ${ props => props.startIcon ? '0.5em' : props.endIcon ? '9em' : 'none' };
+	position: relative;
+	// top: 1.65em;
+	right: ${ props => props.startIcon ? '9em' : props.endIcon ? '1.5em' : 'none' };
 `;
 
 const HelperText = styled.span`
-  display: ${ props => props.helperText ? 'none' : 'block' };
-  font-family: 'Noto Sans JP', sans-serif;
-  font-weight: 400;
-  font-size: 0.65em;
-  margin-top: 0.2em;
-  color: ${props => setErrorColor( props )};
+	display: ${ props => props.helperText ? 'none' : 'block' };
+	font-family: 'Noto Sans JP', sans-serif;
+	font-weight: 400;
+	font-size: 0.65em;
+	margin-top: 0.2em;
+	color: ${props => setErrorColor( props )};
 `;
 
+const InputContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-start;
+	align-items: center;
+`
+
 const Input = ({ startIcon, endIcon, error, label, helperText, ...props }) => {
-  return (
-    <Section>
-      <Label error={ error }>{ label }</Label>
-      { startIcon 
-        ? <Icon startIcon={ true } className="material-icons">{ startIcon }</Icon> 
-        : endIcon 
-        ? <Icon endIcon={ true } className="material-icons">{ endIcon }</Icon>
-        : undefined
-      }
-      <StyledInput error={ error } { ...props } />
-      <HelperText error={ error }>{ helperText }</HelperText>
-    </Section>
-  )
+	return (
+		<Section>
+			<Label error={ error }>{ label }</Label>
+			<InputContainer>
+				<StyledInput startIcon={ startIcon } endIcon={ endIcon } error={ error } { ...props } />
+				{ startIcon 
+					? <Icon startIcon={ true } className="material-icons">{ startIcon }</Icon> 
+					: endIcon 
+					? <Icon endIcon={ true } className="material-icons">{ endIcon }</Icon>
+					: undefined
+				}
+			</InputContainer>
+			<HelperText error={ error }>{ helperText }</HelperText>
+		</Section>
+	)
 };
 
 export { Input };
