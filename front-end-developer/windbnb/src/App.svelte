22704122b8574fb219989dc36b01dbb3076e1aaf
Amy Dang <kirontoo@gmail.com>
2021-06-19 14:44:12 -0700

    add cards to homepage

--- front-end-developer/windbnb/src/App.svelte
+++ front-end-developer/windbnb/src/App.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
-import Button from './components/Button.svelte';
-import Header from './components/Header.svelte';
-
-export let name: string = "Evan";
-let src ="favicon.png";
+  import Header from './components/Header.svelte';
+  import Input from './components/Input.svelte';
+  import Card from './components/Card.svelte';
+  import data from './stays.json'
 </script>
 
 <Header/>
-<main>
-  <h1>Hello {name}</h1>
-  <img src={src} alt="">
-  <Button outline text="text here"/>
+<main role="main">
+  <header class="search">
+    <Input />
+  </header>
+  <section class="locations">
+    <header class="locations-header">
+      <h2>Stays in Finland</h2>
+      <span>12+ stays</span>
+    </header>
+
+    <section class="locations-list">
+      {#each data as item}
+        <Card {...item} locationType={item.type} />
+      {/each}
+    </section>
+  </section>
 </main>
 <footer class="text-sm text-gray">
-created  by 
-  <a class="text-gray underline font-bold" href="https://www.github.com/kirontoo" target="__default"> Kirontoo </a>
+created by 
+  <a class="text-gray underline font-bold" href="https://www.github.com/kirontoo" target="-default"> Kirontoo </a>
   - devChallenges.io
 </footer>
 
@@ -22,4 +33,43 @@ created  by
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  main {
+    @apply m-auto;
+    @apply my-6;
+    @apply flex;
+    @apply flex-col;
+    @apply justify-center;
+    @apply items-center;
+  }
+
+  .search {
+    @apply w-5/6;
+    @apply m-auto;
+  }
+
+  .locations {
+    @apply px-4;
+    @apply my-4;
+    @apply w-full;
+  }
+
+  .locations-header {
+    @apply flex;
+    @apply justify-between;
+    @apply items-center;
+    @apply mb-4;
+  }
+
+  span {
+    @apply text-gray-dark;
+  }
+
+  h2 {
+    @apply font-bold;
+  }
+
+  footer {
+    @apply m-4;
+  }
 </style>
