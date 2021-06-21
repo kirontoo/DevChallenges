0226dcb419fefdda863c2fc3a8a6b6e7dfb9a0ff
Amy Dang <kirontoo@gmail.com>
2021-06-21 15:40:23 -0700

    Make component simpler

--- front-end-developer/windbnb/src/components/Input.svelte
+++ front-end-developer/windbnb/src/components/Input.svelte
@@ -1,15 +1,47 @@
 <script lang="ts">
+  export let grouped: boolean = false;
+  export let placeholder = '';
+  export let value = '';
   import type { Location } from '../global';
   import Button from './Button.svelte';
-  export let location: Location = { city: "Helsinki", country: "Finland" };
+  export let inputValue= '';
 
+// TODO: make option with grouped like in button
+// TODO: make this a simple input component with options for borders
 </script>
 
+<!--
 <section>
-  <input class="location" type="text" placeholder="Location" value={`${location.city}, ${location.country}`}>
-  <input class="guests" type="text" placeholder="Add guests">
-  <button class="search-btn material-icons">search</button>
+  <input 
+    on:input
+    on:change
+    on:click
+    class="location"
+    type="text"
+    placeholder="Location"
+    value={`${value.city}, ${value.country}`}>
+  <input 
+    on:input
+    on:change
+    on:click
+    class="guests"
+    type="text"
+    placeholder="Add guests">
+  <button 
+    on:click
+    class="search-btn material-icons">
+    search
+  </button>
 </section>
+-->
+  <input 
+    bind:value
+    on:input
+    on:change
+    on:click
+    class="guests"
+    type="text"
+    {placeholder}>
 
 <style>
   section {
