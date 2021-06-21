810da57b04fdf5c73ba2e3ae067394c525ab90a0
Amy Dang <kirontoo@gmail.com>
2021-06-21 15:39:44 -0700

    add new component SearchBar

---
+++ front-end-developer/windbnb/src/components/SearchBar.svelte
@@ -0,0 +1,45 @@
+<script>
+import Input from './Input.svelte';
+import Button from './Button.svelte';
+export let location = '';
+
+const handleLocationInput = ( event ) => {
+  console.log(event.target.value);
+}
+</script>
+
+
+<header class="search">
+  <section>
+    <Input 
+      on:input={handleLocationInput}
+      value={location}
+      placeholder="Location"/>
+    <Input 
+      on:input={handleLocationInput} 
+      placeholder="Add guests"/>
+    <Button 
+      grouped
+      color="red"
+      background="white-light"
+      icon="search"/>
+  </section>
+</header>
+
+<style>
+.search {
+  @apply m-auto;
+  @apply w-5/6;
+}
+
+section {
+  font-family: 'Mulish', sans-serif;
+  font-size: 0.4rem;
+  @apply flex;
+  @apply flex-row;
+  @apply w-full;
+  @apply rounded-xl;
+  @apply shadow-md;
+}
+
+</style>
