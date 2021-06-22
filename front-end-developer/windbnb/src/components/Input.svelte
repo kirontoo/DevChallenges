6985727ad3188e0486256eb170eef32c85338f2b
Amy Dang <kirontoo@gmail.com>
2021-06-22 10:38:56 -0700

    add classes

--- front-end-developer/windbnb/src/components/Input.svelte
+++ front-end-developer/windbnb/src/components/Input.svelte
@@ -1,60 +1,23 @@
 <script lang="ts">
-  export let grouped: boolean = false;
+  export let grouped     = false;
+  export let borders     = false;
   export let placeholder = '';
-  export let value = '';
-  import type { Location } from '../global';
-  import Button from './Button.svelte';
-  export let inputValue= '';
-
-// TODO: make option with grouped like in button
-// TODO: make this a simple input component with options for borders
+  export let value       = '';
 </script>
 
-<!--
-<section>
   <input 
     on:input
     on:change
     on:click
-    class="location"
-    type="text"
-    placeholder="Location"
-    value={`${value.city}, ${value.country}`}>
-  <input 
-    on:input
-    on:change
-    on:click
-    class="guests"
-    type="text"
-    placeholder="Add guests">
-  <button 
-    on:click
-    class="search-btn material-icons">
-    search
-  </button>
-</section>
--->
-  <input 
-    bind:value
-    on:input
-    on:change
-    on:click
-    class="guests"
+    class:grouped
+    class:borders
+    {value}
     type="text"
     {placeholder}>
 
 <style>
-  section {
-    font-family: 'Mulish', sans-serif;
-    font-size: 0.4rem;
-    @apply flex;
-    @apply flex-row;
-    @apply w-full;
-    @apply rounded-xl;
-    @apply shadow-md;
-  }
-
   input {
+    @apply p-3;
     @apply w-1/2;
   }
 
@@ -62,21 +25,13 @@
     @apply text-center;
   }
 
-  .location {
-    @apply rounded-l-xl;
-  }
-
-  .guests {
-    @apply border-r;
-    @apply border-l;
+  .borders {
+    @apply border-r-2;
+    @apply border-l-2;
     @apply border-gray-light;
   }
 
-  .search-btn {
-    @apply text-red;
-  }
-
-  input, .search-btn {
-    @apply p-3;
+  .grouped {
+    @apply rounded-l-xl;
   }
 </style>
